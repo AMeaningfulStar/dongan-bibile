5606dc7cd2a29261ab6b771e56a25ea6e85e14a4
A Meaningful star <ghrlfehd123@naver.com>
2024-07-16 17:15:40 +0900

    edit: Login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,52 @@
 'use client'
 
+import Image from 'next/image'
 import Link from 'next/link'
 
-import { LoginPageLayout } from '@/components/Layout'
+import LOGIN_BACKGROUND from '@image/Login_Background.svg'
+import LOGIN_BOTTOM from '@image/Login_Bottom.svg'
 
-export type DatePiece = Date | null
-export type SelectedDate = DatePiece | [DatePiece, DatePiece]
-
-export default function Home() {
+export default function Login() {
   return (
-    <LoginPageLayout pageName="로그인">
-      <div className="relative h-full w-full">
-        {/* FIXME: 배경 이미지로 변경 */}
-        <div className="h-full w-full bg-slate-400"></div>
-        <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
-          <div className="flex flex-col items-center">
-            <div className="mb-10 flex w-52 flex-col gap-y-3.5">
-              <div className="flex items-center justify-between">
-                <label htmlFor="userID" className="text-xl font-light leading-none">
-                  ID
-                </label>
-                <input
-                  id="userID"
-                  className="h-7 border border-black pl-0.5 outline-none"
-                  placeholder="아이디를 입력하세요"
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="userPW" className="text-xl font-light leading-none">
-                  PW
-                </label>
-                <input
-                  id="userPW"
-                  className="h-7 border border-black pl-0.5 outline-none"
-                  placeholder="비밀번호를 입력하세요"
-                />
-              </div>
-            </div>
-            {/* FIXME: Link에서 버튼으로 바꾸고 Login 기능 넣어야함 */}
-            <Link href={'/home'} className="mb-10 rounded-lg border border-black bg-white px-10 py-3">
-              로그인
-            </Link>
-            <p className="mb-8 text-base font-light leading-none">버전: 4.0</p>
-            <div className="flex flex-col gap-y-2">
-              <div className="flex gap-x-2">
-                <button className="h-8 w-24 rounded-lg border border-black bg-white">비밀번호 변경</button>
-                <button className="h-8 w-24 rounded-lg border border-black bg-white">로그아웃</button>
-              </div>
-              <div className="flex gap-x-2">
-                <button className="h-8 w-24 rounded-lg border border-black bg-white">이용 가이드</button>
-                <button className="h-8 w-24 rounded-lg border border-black bg-white">문의하기</button>
-              </div>
-            </div>
-          </div>
+    <div className="flex min-h-screen w-full flex-col items-center justify-between bg-black pb-9 pt-10">
+      <Image alt="backbround image" src={LOGIN_BACKGROUND} className="mb-2.5" />
+      <div className="mb-5 flex flex-col gap-y-2.5">
+        <div className="flex min-w-56 items-center justify-between">
+          <label htmlFor="userID" className="text-xl font-light leading-none text-white">
+            ID
+          </label>
+          <input id="userID" className="h-7 pl-1 outline-none" placeholder="아이디를 입력하세요" />
+        </div>
+        <div className="flex items-center justify-between">
+          <label htmlFor="userPW" className="text-xl font-light leading-none text-white">
+            PW
+          </label>
+          <input id="userPW" className="h-7 pl-1 outline-none" placeholder="비밀번호를 입력하세요" />
         </div>
       </div>
-    </LoginPageLayout>
+      <Link href={'/home'} className="mb-5 flex h-8 w-32 items-center justify-center rounded-lg bg-white">
+        <span className="text-sm font-normal leading-none">로그인</span>
+      </Link>
+      <p className="mb-4 text-base font-normal leading-none text-white">버전: 1.0</p>
+      <div className="mb-4 flex flex-col gap-y-4">
+        <div className="flex gap-x-4">
+          <button className="h-8 w-32 rounded-lg bg-white">
+            <span className="text-sm font-normal leading-none">비밀번호 찾기</span>
+          </button>
+          <button className="h-8 w-32 rounded-lg bg-white">
+            <span className="text-sm font-normal leading-none">회원가입</span>
+          </button>
+        </div>
+        <div className="flex gap-x-4">
+          <button className="h-8 w-32 rounded-lg bg-white">
+            <span className="text-sm font-normal leading-none">이용 가이드</span>
+          </button>
+          <button className="h-8 w-32 rounded-lg bg-white">
+            <span className="text-sm font-normal leading-none">문의하기</span>
+          </button>
+        </div>
+      </div>
+      <Image alt="image" src={LOGIN_BOTTOM} />
+    </div>
   )
 }
