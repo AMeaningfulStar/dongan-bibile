6e6f3407aa15741fb06c6ea0586dc008d38a567b
A Meaningful star <ghrlfehd123@naver.com>
2024-08-07 13:23:58 +0900

    edit: 네비게이션 메뉴 수정 및 헤더의 z-index 추가

--- src/components/Layout/DashboardLayout/index.tsx
+++ src/components/Layout/DashboardLayout/index.tsx
@@ -9,7 +9,6 @@ import { Title } from '@/components/Text'
 
 import useFirebaseStore from '@/stores/FirebaseStore'
 
-import BIBLE_ICON from '@icon/bible_icon.svg'
 import CALENDAR_ICON from '@icon/calendar_icon.svg'
 import EVENT_ICON from '@icon/event_icon.svg'
 import LIGHTUP_ICON from '@icon/lightup_icon.svg'
@@ -32,7 +31,7 @@ export function DashboardLayout({ children, pageName }: LayoutType) {
   }, [])
   return (
     <div className="flex min-h-screen w-full flex-col items-center py-24">
-      <div className="fixed left-0 top-0 flex w-full items-center justify-center border-b border-[#AAAAAA] bg-white pb-3 pt-12">
+      <div className="fixed left-0 top-0 z-30 flex w-full items-center justify-center border-b border-[#AAAAAA] bg-white pb-3 pt-12">
         <Title textColor="">{pageName}</Title>
       </div>
       {children}
@@ -100,9 +99,6 @@ function NavigationBar() {
       </Link>
       <Link href={'/meditation'} className="px-6 py-3">
         <Image alt="button" src={MEDITATION_ICON} />
-      </Link>
-      <Link href={'/bible'} className="px-6 py-3">
-        <Image alt="button" src={BIBLE_ICON} />
       </Link>
       <Link href={'/status'} className="px-6 py-3">
         <Image alt="button" src={STATUS_ICON} />
