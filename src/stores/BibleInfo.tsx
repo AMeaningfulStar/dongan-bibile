30f34b2a2615bd4f4ba571991625dd332f64b303
A Meaningful star <ghrlfehd123@naver.com>
2024-08-07 13:02:03 +0900

    edit: DB에 있는 성경을 불러 오기 위한 설정값 store

---
+++ src/stores/BibleInfo.tsx
@@ -0,0 +1,30 @@
+import { create } from 'zustand'
+
+import { BibleType } from '@/libs/bibleType'
+
+interface BibleStore {
+  datePick: string
+  bibleType: BibleType
+  setDatePick: (newDatePick: string) => void
+  setBibleType: (newBibleType: BibleType) => void
+}
+
+const useBibleInfo = create<BibleStore>((set) => ({
+  datePick: '',
+
+  bibleType: BibleType.REVISED,
+
+  setDatePick: (newDatePick: string) =>
+    set((state: BibleStore) => ({
+      ...state,
+      datePick: newDatePick,
+    })),
+
+  setBibleType: (newBibleType: BibleType) =>
+    set((state: BibleStore) => ({
+      ...state,
+      bibleType: newBibleType,
+    })),
+}))
+
+export default useBibleInfo
