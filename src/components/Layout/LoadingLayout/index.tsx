455281511c2d616ef82ba3041f63de136537b077
A Meaningful star <ghrlfehd123@naver.com>
2024-08-07 13:15:49 +0900

    edit: 성경 정보를 불러오는 중 페이지인 LoadingLayout component

---
+++ src/components/Layout/LoadingLayout/index.tsx
@@ -0,0 +1,13 @@
+import LIGHTUP_ICON from '@icon/lightup_icon.svg'
+import Image from 'next/image'
+
+export function LoadingLayout() {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center py-24">
+      <div className="flex flex-grow flex-col items-center justify-center gap-y-5">
+        <Image alt="image" src={LIGHTUP_ICON} />
+        <div className="text-xl leading-none">로딩 중...</div>
+      </div>
+    </div>
+  )
+}
