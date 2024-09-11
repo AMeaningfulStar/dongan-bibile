22849d4262d9fe504e698e4ef41f22750b8a5ff5
A Meaningful star <ghrlfehd123@naver.com>
2024-09-11 14:27:49 +0900

    edit: Version component

---
+++ src/components/Text/Version/index.tsx
@@ -0,0 +1,9 @@
+import { twMerge } from 'tailwind-merge'
+
+interface VersionType {
+  marginBottom: string
+  textColor: string
+}
+export function Version({ marginBottom, textColor }: VersionType) {
+  return <p className={twMerge('text-base font-normal leading-none', marginBottom, textColor)}>버전: 2.0.3</p>
+}
