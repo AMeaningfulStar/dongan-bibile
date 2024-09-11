b51ac7191b37820ea631cd0c7727fcb04de99d1a
A Meaningful star <ghrlfehd123@naver.com>
2024-09-11 14:27:21 +0900

    refactor: main page - apply ProgressBar components

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -16,10 +16,9 @@ import { DashboardLayout } from '@/components/Layout'
 import { ConsecutiveDays } from '@/components/MainPage'
 
 import { Label } from '@/components/Text'
+import { ChallengeProgressBar, MyProgressBar } from '@/components/Progress'
 import useBibleInfo from '@/stores/BibleInfo'
-import AIRPLANE_ICON from '@icon/airplane_icon.svg'
 import BIBLE_ICON from '@icon/bible.svg'
-import TRAFFICLIGHT_ICON from '@icon/trafficLight_icon.svg'
 
 export default function Main() {
   const { firebaseInfo, setFirebaseUid, setFirebaseInfo, initFirebaseInfo } = useFirebaseStore()
@@ -36,45 +35,6 @@ export default function Main() {
       .catch((error) => {
         console.error('Error checking for duplicate email:', error)
       })
-  }
-
-  // 청신호 진행률 계산
-  const calculateChallengeProgress = () => {
-    const start = new Date('2024-08-11').getTime()
-    const end = new Date('2024-12-21').getTime()
-    const today = new Date().getTime()
-
-    const totalDuration = (end - start) / (1000 * 60 * 60 * 24) // 전체 기간 (일 단위)
-    const elapsedDuration = (today - start) / (1000 * 60 * 60 * 24) // 경과한 기간 (일 단위)
-
-    if (today < start) {
-      return 0
-    }
-
-    const progress = (elapsedDuration / totalDuration) * 100
-
-    return Math.round(progress)
-  }
-
-  // 나의 진행률 계산
-  const calculateMyProgress = () => {
-    const start = new Date('2024-08-11').getTime()
-    const end = new Date('2024-12-21').getTime()
-    const totalDuration = (end - start) / (1000 * 60 * 60 * 24) + 1
-
-    if (firebaseInfo.bibleReadingDates?.length) {
-      // start와 end 사이의 날짜만 필터링
-      const validDates = firebaseInfo.bibleReadingDates.filter((date) => {
-        const currentDate = new Date(date).getTime()
-        return currentDate >= start && currentDate <= end
-      })
-
-      const result = (validDates.length / totalDuration) * 100
-
-      return Math.round(result)
-    }
-
-    return 0
   }
 
   const goToBibleReading = async (datePick: string) => {
@@ -142,29 +102,9 @@ export default function Main() {
         />
       </div>
       {/* 청신호 진행률 */}
-      <div className="mb-3 flex w-full flex-col gap-y-3 px-4">
-        <div className="pt-5">
-          <Label label="청신호 진행률" imageSrc={TRAFFICLIGHT_ICON} imageAlt="icon" />
-        </div>
-        <div className="flex items-center gap-x-2">
-          <div className="relative h-2 flex-grow bg-[#E8EEFF]">
-            <div className="absolute h-full bg-[#0276F9]" style={{ width: `${calculateChallengeProgress()}%` }}></div>
-          </div>
-          <div>{calculateChallengeProgress()}%</div>
-        </div>
-      </div>
+      <ChallengeProgressBar />
       {/* 나의 진행률 */}
-      <div className="mb-16 flex w-full flex-col gap-y-3 px-4">
-        <div className="pt-5">
-          <Label label="나의 진행률" imageSrc={AIRPLANE_ICON} imageAlt="icon" />
-        </div>
-        <div className="flex items-center gap-x-2">
-          <div className="relative h-2 flex-grow bg-[#E8EEFF]">
-            <div className="absolute h-full bg-[#0276F9]" style={{ width: `${calculateMyProgress()}%` }}></div>
-          </div>
-          <div>{calculateMyProgress()}%</div>
-        </div>
-      </div>
+      <MyProgressBar />
       {/* 버전 */}
       <p className="mb-6 text-base font-normal leading-none">버전: 2.0.2</p>
       {/* 버튼 2개 */}
