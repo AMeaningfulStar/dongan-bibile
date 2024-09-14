3d97e4ca8aa8ff3a86fb0d45467dbedb892c1736
A Meaningful star <ghrlfehd123@naver.com>
2024-09-14 14:56:21 +0900

    edit: bible page - text size change buttom UI

--- src/app/bible/page.tsx
+++ src/app/bible/page.tsx
@@ -186,6 +186,14 @@ export default function Bible() {
       >
         말씀을 읽었습니다
       </button>
+      <div className="fixed bottom-20 right-3 flex gap-x-2">
+        <button className="h-10 w-10 rounded-full border-4 border-[#0276F9] border-opacity-60 bg-[#A7D2FF] bg-opacity-60 text-2xl font-bold leading-none text-[#0276F9] text-opacity-60">
+          +
+        </button>
+        <button className="h-10 w-10 rounded-full border-4 border-[#AAA] border-opacity-60 bg-[#CCC] bg-opacity-60 text-2xl font-bold leading-none text-[#AAA] text-opacity-60">
+          -
+        </button>
+      </div>
     </DashboardLayout>
   )
 }
