98feedd37363a33d4c1f778ee50c0b8405374b9f
A Meaningful star <ghrlfehd123@naver.com>
2024-10-29 15:28:28 +0900

    edit: progressBar component - chapter text fix

--- src/components/Progress/ChallengeProgressBar/index.tsx
+++ src/components/Progress/ChallengeProgressBar/index.tsx
@@ -7,7 +7,7 @@ export function ChallengeProgressBar({ progressResult }: { progressResult: { pro
     <div className="mb-3 flex w-full flex-col gap-y-3 px-4">
       <div className="flex items-center gap-x-2 pt-5">
         <Label label="청신호 진행률" imageSrc={TRAFFICLIGHT_ICON} imageAlt="icon" />
-        <span className="font-extralight">( {progressResult.chapters}장 / 206장 )</span>
+        <span className="font-extralight">( {progressResult.chapters}장 / 260장 )</span>
       </div>
       <div className="flex items-center gap-x-2">
         <div className="relative h-2 flex-grow bg-[#E8EEFF]">
--- src/components/Progress/MyProgressBar/index.tsx
+++ src/components/Progress/MyProgressBar/index.tsx
@@ -7,7 +7,7 @@ export function MyProgressBar({ progressResult }: { progressResult: { progress: 
     <div className="mb-16 flex w-full flex-col gap-y-3 px-4">
       <div className="flex items-center gap-x-2 pt-5">
         <Label label="나의 진행률" imageSrc={AIRPLANE_ICON} imageAlt="icon" />
-        <span className="font-extralight">( {progressResult.chapters}장 / 206장 )</span>
+        <span className="font-extralight">( {progressResult.chapters}장 / 260장 )</span>
       </div>
       <div className="flex items-center gap-x-2">
         <div className="relative h-2 flex-grow bg-[#E8EEFF]">
