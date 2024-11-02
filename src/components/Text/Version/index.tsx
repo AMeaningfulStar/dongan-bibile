9e62acb878380a9e75f936443ebcd2d857a485f4
A Meaningful star <ghrlfehd123@naver.com>
2024-11-02 22:26:18 +0900

    edit: 버전 관리

--- src/components/Text/Version/index.tsx
+++ src/components/Text/Version/index.tsx
@@ -5,5 +5,5 @@ interface VersionType {
   textColor: string
 }
 export function Version({ marginBottom, textColor }: VersionType) {
-  return <p className={twMerge('text-base font-normal leading-none', marginBottom, textColor)}>버전: 2.2.0</p>
+  return <p className={twMerge('text-base font-normal leading-none', marginBottom, textColor)}>버전: 3.0.0</p>
 }
