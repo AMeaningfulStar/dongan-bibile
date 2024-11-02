a1ddcda91cd3451fc6c81955379550e8754caf64
A Meaningful star <ghrlfehd123@naver.com>
2024-11-02 22:17:37 +0900

    edit: status page - apply ClassReadingStatus&GlobalReadingStatus component

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -1,106 +1,57 @@
-import { LoadingScreen } from '@/components/Layout'
+'use client'
+import { ko } from 'date-fns/locale'
+import moment from 'moment'
+import { forwardRef } from 'react'
+import DatePicker from 'react-datepicker'
+
+import { DashboardLayout } from '@/components/Layout'
+import { ClassReadingStatus, GlobalReadingStatus } from '@/components/StatusPage'
+
+import useBibleInfo from '@/stores/BibleInfo'
+
+import 'react-datepicker/dist/react-datepicker.css'
 
 export default function Status() {
+  const { datePick, setDatePick } = useBibleInfo()
+
+  const PickerCustomInput = forwardRef(({ value, onClick }: any, ref: any) => (
+    <button className="h-full flex-grow" onClick={onClick} ref={ref}>
+      {value}
+    </button>
+  ))
+
   return (
-    <LoadingScreen pageName="읽기현황"></LoadingScreen>
-    // <div className="flex min-h-screen w-full flex-col items-center py-20">
-    //   <div className="fixed left-0 top-0 flex w-full items-center justify-center border-b border-[#AAAAAA] bg-white pb-3 pt-10">
-    //     <span className="text-xl font-light">읽기현황</span>
-    //   </div>
-    //   {/* 세팅 및 정보 */}
-    //   <div className="flex w-full justify-between gap-x-3 px-4 py-3">
-    //     <div className="flex flex-grow gap-x-2.5">
-    //       <div className="text-base">날짜</div>
-    //       <button className="flex-grow rounded-md border border-black">00 / 00</button>
-    //     </div>
-    //     <div className="flex flex-grow gap-x-2.5">
-    //       <div className="text-base">오늘 읽은 사람</div>
-    //       <div className="flex flex-grow items-center justify-center rounded-md border border-black">
-    //         <span>00명</span>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   {/* 반 리스트 및 명단 */}
-    //   <div className="flex w-full flex-col gap-y-2.5">
-    //     <div className="w-full">
-    //       <div className="flex justify-between bg-[#E8EEFF] px-4 py-2 text-lg leading-none">
-    //         <span className="flex-grow text-center">1-1</span>
-    //         <span>(#/#)</span>
-    //       </div>
-    //       <div className="flex flex-wrap justify-center gap-x-1.5 gap-y-2.5 py-4">
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //       </div>
-    //     </div>
-    //     <div className="w-full">
-    //       <div className="flex justify-between bg-[#E8EEFF] px-4 py-2 text-lg leading-none">
-    //         <span className="flex-grow text-center">2-4</span>
-    //         <span>(#/#)</span>
-    //       </div>
-    //       <div className="flex flex-wrap justify-center gap-x-1.5 gap-y-2.5 py-4">
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //       </div>
-    //     </div>
-    //     <div className="w-full">
-    //       <div className="flex justify-between bg-[#E8EEFF] px-4 py-2 text-lg leading-none">
-    //         <span className="flex-grow text-center">교사</span>
-    //         <span>(#/#)</span>
-    //       </div>
-    //       <div className="flex flex-wrap justify-center gap-x-1.5 gap-y-2.5 py-4">
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //         <div className="rounded-lg bg-[#EEEEEE] px-4 py-2">김이박최</div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   {/* 하단 네비게이션 바 */}
-    //   <div className="fixed bottom-0 left-0 flex w-full justify-between border-t bg-white pb-9">
-    //     <Link href={'/home'} className="px-6 py-3">
-    //       <Image alt="button" src={CALENDAR_ICON} />
-    //     </Link>
-    //     <Link href={'/meditation'} className="px-6 py-3">
-    //       <Image alt="button" src={MEDITATION_ICON} />
-    //     </Link>
-    //     <Link href={'/bible'} className="px-6 py-3">
-    //       <Image alt="button" src={BIBLE_ICON} />
-    //     </Link>
-    //     <Link href={'/status'} className="px-6 py-3">
-    //       <Image alt="button" src={STATUS_ICON} />
-    //     </Link>
-    //     <Link href={'/event'} className="px-6 py-3">
-    //       <Image alt="button" src={EVENT_ICON} />
-    //     </Link>
-    //   </div>
-    // </div>
+    <DashboardLayout pageName="읽기현황">
+      <div className="flex w-full justify-between gap-x-3 px-4 py-3">
+        <div className="flex flex-grow gap-x-2.5">
+          <div className="text-base">날짜</div>
+          <DatePicker
+            dateFormat="MM월 dd일" // 날짜 형태
+            locale={ko}
+            shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
+            customInput={<PickerCustomInput />}
+            minDate={new Date('2024-07-01')} // minDate 이전 날짜 선택 불가
+            selected={
+              moment(datePick, 'YYYY-MM-DD', true).isValid() ? moment(datePick, 'YYYY-MM-DD').toDate() : new Date()
+            }
+            onChange={(date) => setDatePick(moment(date).format('YYYY-MM-DD'))}
+          />
+        </div>
+        <GlobalReadingStatus />
+      </div>
+      {/* 반 리스트 및 명단 */}
+      <div className="flex w-full flex-col gap-y-2.5">
+        <ClassReadingStatus gradeNum={1} classNum={1} title="1학년 1반" />
+        <ClassReadingStatus gradeNum={1} classNum={2} title="1학년 2반" />
+        <ClassReadingStatus gradeNum={1} classNum={3} title="1학년 3반" />
+        <ClassReadingStatus gradeNum={1} classNum={4} title="1학년 4반" />
+        <ClassReadingStatus gradeNum={1} classNum={5} title="1학년 5반" />
+        <ClassReadingStatus gradeNum={2} classNum={1} title="2학년 1반" />
+        <ClassReadingStatus gradeNum={2} classNum={2} title="2학년 2반" />
+        <ClassReadingStatus gradeNum={2} classNum={3} title="2학년 3반" />
+        <ClassReadingStatus gradeNum={2} classNum={4} title="2학년 4반" />
+        <ClassReadingStatus gradeNum={3} classNum={3} title="청소년 2부 교사" />
+      </div>
+    </DashboardLayout>
   )
 }
