df4ff0fecee51b3ce3ebf35fc0c2f368ca1490e6
A Meaningful star <ghrlfehd123@naver.com>
2024-12-05 16:55:29 +0900

    edit: BibleReadingBtn을 Button 컴포넌트 항목에 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+export { BibleReadingBtn } from './BibleReadingBtn'
 export { KakaoShareBtn } from './KakaoShareBtn'
 export { PrayerConfirmationButton } from './PrayerConfirmationButton'
 export { TextSizeAdjuster } from './TextSizeAdjuster'
