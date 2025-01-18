c858025a9d4e2a4859b8ff932ce0a9cb9ddfa378
A Meaningful star <ghrlfehd123@naver.com>
2025-01-18 15:53:56 +0900

    refactor: 리펙토링을 위한 프로젝트 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,16 @@ export default {
         'layout-20-l': ['20px', { lineHeight: '100%', fontWeight: '400' }],
         'navigation-14-l': ['14px', { lineHeight: '100%', fontWeight: '400' }],
         'navigation-15-l': ['15px', { lineHeight: '100%', fontWeight: '400' }],
+        'caption-13-l': ['13px', { lineHeight: '100%', fontWeight: '400' }],
+        'caption-14-l': ['14px', { lineHeight: '100%', fontWeight: '400' }],
+        'caption-14-m': ['14px', { lineHeight: '100%', fontWeight: '500' }],
+        'caption-14-b': ['14px', { lineHeight: '100%', fontWeight: '700' }],
+        'caption-15-l': ['15px', { lineHeight: '100%', fontWeight: '400' }],
+        'caption-15-m': ['15px', { lineHeight: '100%', fontWeight: '500' }],
+        'caption-16-l': ['15px', { lineHeight: '100%', fontWeight: '400' }],
+        'caption-16-sb': ['16px', { lineHeight: '100%', fontWeight: '600' }],
+        'caption-16-b': ['16px', { lineHeight: '100%', fontWeight: '700' }],
+        'caption-18-sb': ['18px', { lineHeight: '100%', fontWeight: '600' }],
       },
 
       backgroundImage: {
@@ -22,17 +32,15 @@ export default {
       colors: {
         gl: {
           'grayscale-base': 'rgba(238, 238, 238, 1)',
-          'grayscale-1': 'rgba(140, 140, 140, 1)',
-          'grayscale-100': 'rgba(245, 245, 245, 1)',
+          'grayscale-100': 'rgba(140, 140, 140, 1)',
           'grayscale-200': 'rgba(158, 158, 158, 1)',
-          'grayscale-300': 'rgba(128, 128, 128, 1)',
-          'grayscale-opacity-50': 'rgba(158, 158, 158, 0.5)',
           'green-base': 'rgba(13, 152, 0, 1)',
           'green-opacity-30': 'rgba(13, 152, 0, 0.3)',
           'green-opacity-50': 'rgba(13, 152, 0, 0.5)',
           'red-base': 'rgba(255, 0, 0, 1)',
           'white-base': 'rgba(254, 254, 254, 1)',
           'black-base': 'rgba(34, 34, 34, 1)',
+          'black-opacity-30': 'rgba(34, 34, 34, 0.3)',
         },
       },
 
