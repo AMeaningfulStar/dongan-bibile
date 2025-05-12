df8a2aea62c2426c5ac977adad3a8dae20c7ec19
A Meaningful star <ghrlfehd123@naver.com>
2025-05-12 17:24:03 +0900

    refactor: Progress components
    시즌 별로 진행률을 나타내는 MyProgressBar, 전체 진행률을 나타내는 TotalProgressBar

---
+++ src/components/Progress/MyProgressBar.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+export function MyProgressBar({
+  seasonName,
+  seasonProgress,
+  totalSeasonDays,
+  readDatesInSeason,
+}: {
+  seasonName: string
+  seasonProgress: number
+  totalSeasonDays: number
+  readDatesInSeason: string[]
+}) {
+  return (
+    <div className="mb-10 flex w-full flex-col gap-y-2.5 px-4">
+      <div className="ml-1 flex items-center gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <span className="text-caption-16-l">[{seasonName}]</span>
+        </div>
+        <span className="text-caption-13-l">
+          {totalSeasonDays}일 중 {readDatesInSeason.length}일 통독 완료
+        </span>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <div className="relative h-2.5 flex-grow rounded-full bg-gl-green-opacity-30">
+          <div
+            className="absolute h-full rounded-full bg-gl-green-opacity-50"
+            style={{ width: seasonProgress * 100 + '%' }}
+          ></div>
+        </div>
+        <div className="flex w-10 items-center justify-center">{(seasonProgress * 100).toFixed(1) + '%'}</div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Progress/TotalProgressBar.tsx
@@ -0,0 +1,32 @@
+'use client'
+export function TotalProgressBar({
+  totalPlannedDays,
+  readDatesInYear,
+  yearProgress,
+}: {
+  totalPlannedDays: number
+  readDatesInYear: number
+  yearProgress: number
+}) {
+  return (
+    <div className="mb-10 flex w-full flex-col gap-y-2.5 px-4">
+      <div className="ml-1 flex items-center gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <span className="text-caption-16-l">청신호 진행률</span>
+        </div>
+        <span className="text-caption-13-l">
+          {totalPlannedDays} 일 중 {readDatesInYear}일 통독 완료
+        </span>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <div className="relative h-2.5 flex-grow rounded-full bg-gl-green-opacity-30">
+          <div
+            className="absolute h-full rounded-full bg-gl-green-opacity-50"
+            style={{ width: yearProgress * 100 + '%' }}
+          ></div>
+        </div>
+        <div className="flex w-10 items-center justify-center">{(yearProgress * 100).toFixed(1) + '%'}</div>
+      </div>
+    </div>
+  )
+}
