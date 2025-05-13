ea8fbc64f3f413c30514988bbe5ee6122a05691f
A Meaningful star <ghrlfehd123@naver.com>
2025-05-13 11:34:32 +0900

    edit: keyword delete - DELETE

---
+++ src/app/api/keywords/[id]/route.ts
@@ -0,0 +1,29 @@
+import { firestore } from '@/libs/firebase'
+import { deleteDoc, doc } from 'firebase/firestore'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+  try {
+    const keywordId = context.params.id
+    const { searchParams } = new URL(req.url)
+    const date = searchParams.get('datePick')
+    const churchId = searchParams.get('churchId')
+    const communityId = searchParams.get('communityId')
+
+    if (!date || !keywordId) {
+      return NextResponse.json({ status: 400, message: '필수 항목이 누락되었습니다.' })
+    }
+
+    const basePath =
+      churchId && communityId
+        ? `churches/${churchId}/communities/${communityId}/keywords/${date}/keywords/${keywordId}`
+        : `keywords/${date}/keywords/${keywordId}`
+
+    await deleteDoc(doc(firestore, basePath))
+
+    return NextResponse.json({ status: 200, message: '키워드가 삭제되었습니다.' })
+  } catch (error) {
+    console.error('키워드 삭제 오류:', error)
+    return NextResponse.json({ status: 500, message: '서버 오류가 발생했습니다.' })
+  }
+}
