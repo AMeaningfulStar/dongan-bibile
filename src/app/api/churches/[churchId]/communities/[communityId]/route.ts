80460861055e7ae8c24742fb36c6cea533b199ba
A Meaningful star <ghrlfehd123@naver.com>
2025-06-03 18:26:01 +0900

    edit: communities list API - DELETE
    공동체 삭제하는 API

--- src/app/api/churches/[churchId]/communities/[communityId]/route.ts
+++ src/app/api/churches/[churchId]/communities/[communityId]/route.ts
@@ -1,5 +1,5 @@
 import { firestore } from '@/libs/firebase'
-import { doc, updateDoc } from 'firebase/firestore'
+import { deleteDoc, doc, updateDoc } from 'firebase/firestore'
 import { NextRequest, NextResponse } from 'next/server'
 
 interface Params {
@@ -28,5 +28,22 @@ export async function PATCH(req: NextRequest, { params }: Params) {
   } catch (error) {
     console.error('공동체 수정 오류:', error)
     return NextResponse.json({ status: 500, message: '공동체 수정에 실패했습니다.' })
+  }
+}
+
+export async function DELETE(_: NextRequest, { params }: Params) {
+  try {
+    const { churchId, communityId } = params
+
+    if (!churchId || !communityId) {
+      return NextResponse.json({ status: 400, message: '필수 정보가 누락되었습니다.' })
+    }
+
+    await deleteDoc(doc(firestore, 'churches', churchId, 'communities', communityId))
+
+    return NextResponse.json({ status: 200, message: '공동체가 삭제되었습니다.' })
+  } catch (error) {
+    console.error('공동체 삭제 오류:', error)
+    return NextResponse.json({ status: 500, message: '공동체 삭제에 실패했습니다.' })
   }
 }
